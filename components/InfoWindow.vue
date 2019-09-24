655c41423ce9d8b74cf8ede57bae85455cc81686
Farhan <farhantariq12b@gmail.com>
2019-09-24 19:12:55 +0500

    map implemented

---
+++ components/InfoWindow.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="require('~/assets/map/thumbnail.jpeg')" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <p class="card-content-text">
+         GPOCam 
+        <i class="fa fa-external-link-alt font-12"></i>  
+      </p>
+      <div class="divider-line"></div>
+      <div class="card-footer-grid">
+        <div class="card-footer-item">
+          <p class="font-weight-medium">Vendor/Model</p>
+        </div>
+        <div class="card-footer-item">
+          <p>
+            <img :src="require('~/assets/map/hikvision.jpg')" width="60" height="8" alt="Placeholder image">
+            /Hikvision
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['marker'],
+  }
+</script>
+
+<style>
+  .card-image img {
+    width: 300px;
+    height: 169px;
+    margin-top: 18px;
+  }
+  .divider-line {
+    height: 1px;
+    background-color: #ddd;
+    width: 100%;
+  }
+  .card-content-text {
+    text-align: center;
+    margin-top: 5px;
+    font-size: 10px;
+    font-weight: 600;
+    margin-bottom: 7px !important;
+    font-family: "PingFang SC","Helvetica Neue","Hiragino Sans GB","Segoe UI","Microsoft YaHei","微软雅黑",sans-serif;
+    font-weight: normal;
+  }
+  
+  .card-content-text .fa {
+    color: #2a6496;
+    font-size: 12px;
+  }
+  .card-footer-grid {
+    margin-top: 10px;
+    display: grid;
+    grid-template-columns: 40% 60%;
+  }
+  .card-footer-item p {
+    margin-bottom: 7px;
+    font-size: 12px;
+  }
+</style>
--- pages/map.vue
+++ pages/map.vue
@@ -1,7 +1,107 @@
 <template>
   <div>
-    <v-layout>
-      <h2>Map page</h2>
-    </v-layout>
+    <gmap-map
+      :options="{
+        zoomControl: false,
+        mapTypeControl: false,
+      }"
+      ref="gmap"
+      :center="center"
+      :zoom="12"
+      class="map-box"
+    >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        @click="toggleInfoWindow(m,index)">
+      </gmap-marker>
+
+      <gmap-info-window
+        :options="infoOptions"
+        :position="infoWindowPos"
+        :opened="infoWinOpen"
+        @closeclick="infoWinOpen=false"
+      >
+        <InfoWindow :marker="infoContent" />
+      </gmap-info-window>
+    </gmap-map>
   </div>
 </template>
+<script>
+  import InfoWindow from '@/components/InfoWindow';
+  export default {
+    name: "GoogleMap",
+    components: {
+      InfoWindow
+    },
+    data() {
+      return {
+        center: { 
+          lat: 52.511950, 
+          lng: 6.089625 
+        },
+        map: null,
+        infoContent: '',
+        infoWindowPos: {
+          lat: 0,
+          lng: 0
+        },
+        infoWinOpen: false,
+        currentMidx: null,
+        infoOptions: {
+          pixelOffset: {
+            width: 0,
+            height: -35
+          }
+        },
+        markers: [
+          {
+            name: "House of Aleida Greve",
+            position: {lat: 52.512942, lng: 6.089625}
+          },
+          {
+            name: "House of Potgieter",
+            position: {lat: 52.511950, lng: 6.091056}
+          },
+          {
+            name: "House of Johannes Cele",
+            position: {lat: 52.511047, lng: 6.091728}
+          },
+        ],
+      };
+    },
+    mounted() {
+      //set bounds of the map
+      this.$refs.gmap.$mapPromise.then((map) => {
+        const bounds = new google.maps.LatLngBounds()
+        for (let m of this.markers) {
+          bounds.extend(m.position)
+        }
+        map.fitBounds(bounds);
+      });
+    },
+    methods: {
+      toggleInfoWindow: function (marker, idx) {
+        this.infoWindowPos = marker.position;
+        this.infoContent = marker;
+        
+        //check if its the same marker that was selected if yes toggle
+        if (this.currentMidx == idx) {
+          this.infoWinOpen = !this.infoWinOpen;
+        }
+        //if different marker set infowindow to open and reset current marker index
+        else {
+          this.infoWinOpen = true;
+          this.currentMidx = idx;
+        }
+      },
+    }
+  };
+</script>
+<style>
+  .map-box {
+    width:100%;
+    height: 100rem;
+  }
+</style>
