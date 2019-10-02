2da3761dbe7e63fddbc9240aa365dc53b5df14c1
Azhar Malik <azharmalik02@gmail.com>
2019-10-02 13:00:37 +0500

    Added user activities page

--- components/users_tabs.vue
+++ components/users_tabs.vue
@@ -1,25 +1,33 @@
 <template>
-  <v-tabs>
-    <v-tab :to="'/users/account'" nuxt>
-      Account
-    </v-tab>
-    <v-tab :to="'/users/password'" nuxt>
-      Password
-    </v-tab>
-    <v-tab :to="'/users/activities'" nuxt>
-      Activities
-    </v-tab>
-    <v-tab :to="'/users/embedded-live-view'" nuxt>
-      Live View Widget
-    </v-tab>
-    <v-tab :to="'/users/embedded-recordings'" nuxt>
-      Recordings Widget
-    </v-tab>
-    <v-tab :to="'/users/api-call'" nuxt>
-      API
-    </v-tab>
-  </v-tabs>
+  <v-sheet class="tabs" elevation="6">
+    <v-tabs>
+      <v-tab :to="'/users/account'" nuxt>
+        Account
+      </v-tab>
+      <v-tab :to="'/users/password'" nuxt>
+        Password
+      </v-tab>
+      <v-tab :to="'/users/activities'" nuxt>
+        Activities
+      </v-tab>
+      <v-tab :to="'/users/embedded-live-view'" nuxt>
+        Live View Widget
+      </v-tab>
+      <v-tab :to="'/users/embedded-recordings'" nuxt>
+        Recordings Widget
+      </v-tab>
+      <v-tab :to="'/users/api-call'" nuxt>
+        API
+      </v-tab>
+    </v-tabs>
+  </v-sheet>
 </template>
+
+<style scoped>
+.tabs {
+  border-radius: 0 !important;
+}
+</style>
 
 <script>
 export default {
